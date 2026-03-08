4df8a12824bfbce167de0b2efb0e08ef001ce394
tornadoleak <nikitkayt09@gmail.com>
2026-03-08 16:22:14 +0300

    Delete kazino_new_mart directory

--- kazino_new_mart/js/firebase.js
+++
@@ -1,8 +0,0 @@
-    const firebaseConfig = {
-        apiKey: "AIzaSyC8UkaoaI7nHRSxNPddRwXnNxzHX7kt8s8",
-        databaseURL: "https://tornadogame-41ae2-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "tornadogame-41ae2",
-        appId: "1:926174944730:web:20be5b57efb23ab853b4e3"
-    };
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.database(); // ✅ добавь эту строку
